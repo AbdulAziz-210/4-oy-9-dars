3e8180e9a9c2321607bbd2f9a6676d7d6dd018af
lexus600 <LexusToyota2002@gmail.com>
2024-02-14 22:19:19 +0500

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 // 1-masala
 
 
-// 2-masalafesgwe
-wgw
-gwsgw
+// 2-masala
+
+
+
+// //  3-masala
+
+// const numbers = [2, 4, 5, 6, 6, 3, 2, 1]
+// let newNum = []
+
+// function newFunction () {
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (!newNum.includes(numbers[i])) {
+//             newNum.push(numbers[i])
+//         }
+
+//     }
+// }
+// newFunction ()
+// console.log(newNum);
+
+
+// //   4-masala
+
+
